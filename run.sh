77322377ec20afb2201b3074fd4b50c0abc90fd7
fabian <fabian@fabians-MacBook-Pro.local>
2018-09-26 11:13:41 +0700

    changed container name

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker network create --driver bridge --subnet 172.0.0.0/29 jenkins-network &>/d
 
 # Run Docker container
 docker run --publish 443:443 --publish 80:80 \
-  --name nginx-jenkins-CTO  \
+  --name nginx-jenkins-cto  \
   --network="jenkins-network" \
   --detach \
   fabianschyrer/jenkins.docker.cto.nginx:latest
